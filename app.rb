71be3ba72ce581b1976dc2bb613f57a4b4464f69
Jane Costa <yberdysh@gmail.com>
2018-09-18 21:34:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    # {"name"=>"q", "pg"=>"q", "sg"=>"q", "pf"=>"q", "sf"=>"q", "c"=>"q"}
+    # binding.pry
+    erb :team
+  end
 
 end
